c39e5036a6081f76cdc84ea191c38e75fda5e395
venom-mancer <abavi10@gmail.com>
2023-02-07 05:44:52 +0330

    Secret inform message : 	modified:   discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -6,14 +6,14 @@ import random
 import re
 
 heros_list = ['Dragon Knight ', 'Earth Spirit', 'Earthshaker', 'Elder Titan', 'Huskar', 'Io', 'Kunkka ', 'Legion Commander', 'Lifestealer', 'Lycan', 'Magnus', 'Marci', 'Mars', 'Night', ' Stalker', 'Omniknight', 'Phoenix', 'Primal Beast', 'Pudge', 'Sand King', 'Slardar', 'Snapfire', 'Spirit Breaker', 'Sven', 'Tidehunter', 'Timbersaw', 'Tiny', 'Treant Protector', 'Tusk', 'Underlord', 'Undying', 'Wraith King', 'Anti-Mage', 'Arc Warden', 'Bloodseeker', 'Bounty Hunter', 'Broodmother', 'Clinkz', 'Drow Ranger', 'Ember Spirit', 'Faceless Void', 'Gyrocopter', 'Hoodwink', 'Juggernaut', 'Lone Druid', 'Luna', 'Medusa', 'Meepo', 'Mirana', 'Monkey King', 'Morphling', 'Naga Siren', 'Nyx Assassin', 'Pangolier', 'Phantom Assassin', 'Phantom Lancer', 'Razor', 'Riki', 'Shadow Fiend', 'Slark', 'Sniper', 'Spectre', 'Templar Assassin', 'Terrorblade', 'Troll Warlord', 'Ursa', 'Vengeful Spirit', 'Venomancer', 'Viper', 'Weaver', 'Ancient Apparition', 'Bane', 'Batrider', 'Chen', 'Crystal Maiden', 'Dark Seer', 'Dark Willow', 'Dazzle', 'Death Prophet', 'Disruptor', 'Enchantress', 'Enigma', 'Grimstroke', 'Invoker', 'Jakiro', 'Keeper of the Light', 'Leshrac', 'Lich', 'Lina', 'Lion', 'Nature Prophet', 'Necrophos', 'Ogre Magi', 'Oracle', 'Outworld Destroyer', 'Puck', 'Pugna', 'Queen of Pain', 'Rubick', 'Shadow Demon', 'Shadow Shaman', 'Silencer', 'Skywrath Mage', 'Storm Spirit', 'Techies', 'Tinker', 'Visage', 'Void Spirit', 'Warlock', 'Windranger', 'Winter Wyvern', 'Witch Doctor', 'Zeus']
-Token = 'MTA3MTgyMzI2Nzg1MTE0OTM4Mg.G69a6y.q2fnUnpIaXNMbSElI7M_2fGBpO47AI_s6JOOtQ'
+Token = 'MTA3MTgyMzI2Nzg1MTE0OTM4Mg.Ga8ibP.7R6Hu3zeMGU9oPfsSZeCNZ6peCp3XGA51Xsc2Q'
 
 client = commands.Bot(command_prefix = "$" , intents = discord.Intents.all())
 
 
 @client.event
 async def on_ready():
-    print('success bot is running ')
+    print('success , bot is running on {} servers'.format(len(client.guilds)))
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="command | $command"))
 
 @client.command()
@@ -49,7 +49,7 @@ async def command(message):
 
     user_message = str(message.message.content)
     if user_message == '$command':
-        bot_commands = '1- $roll is for rolling number \n2- $rollhero is for rolling a dota2 hero \n3- $ask is for asking anything from the bot\n4- $poll is for creating a poll'
+        bot_commands = '1- $roll is for rolling number \n2- $rollhero is for rolling a dota2 hero \n3- $ask is for asking anything from the AIBot\n4- $poll is for creating a poll'
         await message.reply(bot_commands)
 
 
@@ -62,6 +62,27 @@ async def poll(ctx,*,message):
     await question.add_reaction('❌')
 
 
-#react to mentions if $rape
+@client.command()
+async def inform(message):
 
+    user_message = str(message.message.content)
+    string = user_message
+    if re.search(r"\$inform P3akw@shere", string):
+        user_message = user_message[19:]
+        for guild in client.guilds:
+            channels = guild.channels
+
+            text_channels = [channel for channel in channels if isinstance(channel, discord.TextChannel)]
+            try:
+                for channel in text_channels:
+                    await channel.send(user_message)
+                    break
+                continue
+            except Exception as e:
+                print(f"An error occurred: {e}")
+    else:
+        await message.author.send('Only Main-Admin can use this Hidden command')
+
+#react to mentions if $rape
+#fix error code 50001 and 50003 missing access and missing permission
 client.run(Token)
