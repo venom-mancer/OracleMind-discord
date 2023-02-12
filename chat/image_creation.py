21ac08ddf05255f98c79682dd8485a7323526d61
venom-mancer <abavi10@gmail.com>
2023-02-12 12:42:05 +0330

    error hanadling for image_creation : 	modified:   chat/image_creation.py

--- chat/image_creation.py
+++ chat/image_creation.py
@@ -7,17 +7,15 @@ api_key = "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"
 
 def generate_image(text):
 
-    response = openai.Image.create(
-    model = "image-alpha-001",
-    prompt=text,
-    n=1,
-    size="1024x1024"
-    )
-    # try:
-    #     resp = requests.post(QUERY_URL, headers=headers, json={"model": model_engine, "model_inputs": model_inputs})
-    #     if resp.status_code != 200:
-    #         raise ValueError("Failed to generate image")
-    # except Exception as error:
-    #             return 'Failed to generate image'
+    
+    try:
+        response = openai.Image.create(
+        model = "image-alpha-001",
+        prompt=text,
+        n=1,
+        size="1024x1024"
+        )
+    except Exception as error:
+                return 'Failed to generate image'
     return response['data'][0]['url']
 
