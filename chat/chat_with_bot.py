b7b16905843fbbcd6c9b23443f46d6ec643870f7
venom-mancer <abavi10@gmail.com>
2024-04-04 17:09:47 +0330

    New question answer method soon: 	modified:   chat/chat_with_bot.py 	modified:   chat/translator.py

--- chat/chat_with_bot.py
+++ chat/chat_with_bot.py
@@ -1,26 +1,26 @@
-import openai
+#Deploying new question answer method
+
 import json
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+
+
+model_name = "deepset/roberta-base-squad2"
+
 
 with open('config.json') as user_file:
   file_contents = user_file.read()
 parsed_json = json.loads(file_contents)
 
-openai.api_key = parsed_json["open_ai_token"]
-openai.Model.list()
-
-
 def talktobot(chat):
 
+  nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
 
-    response = openai.Completion.create(
 
-        engine="text-davinci-003",
-        prompt = "<|endoftext|>"+chat+"\n--\nLabel:",
-        temperature=0.8,
-        top_p=0.9,
-        max_tokens=750,
-    )
+  res = res["answer"]
+  return res
 
-    output_label = response["choices"][0]["text"]
 
-    return output_label
+model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+#
--- chat/translator.py
+++ chat/translator.py
@@ -1,17 +1,2 @@
-import googletrans
-from googletrans import Translator
-
-supported_lang = ['fa']
-
-def eng_translator(text):
-
-    translator = Translator()
-    
-    lang_detector = translator.detect(text).lang
-    if lang_detector in supported_lang:
-        return text
-    else:
-
-        translated = translator.translate(text , src='en',dest=supported_lang[0]).text
-        return translated
+#Deploying new translator.
     
